75fdefe2bbd1d2b5d1af7bf89c9f76179d05a664
QuangHM <quanghm@vng.com.vn>
2015-11-18 17:12:04 +0700

    delete build

--- _build/ble_sensorsim.d
+++
@@ -1,7 +0,0 @@
-_build/ble_sensorsim.o: Source/ble/ble_sensorsim.c \
- Include/ble/ble_sensorsim.h \
- /usr/local/gcc-arm-none-eabi-4_8-2014q1/lib/gcc/arm-none-eabi/4.8.3/include/stdint.h \
- /usr/local/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/stdint.h \
- /usr/local/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/machine/_default_types.h \
- /usr/local/gcc-arm-none-eabi-4_8-2014q1/arm-none-eabi/include/sys/features.h \
- /usr/local/gcc-arm-none-eabi-4_8-2014q1/lib/gcc/arm-none-eabi/4.8.3/include/stdbool.h
